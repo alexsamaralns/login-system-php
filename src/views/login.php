22eeca165f49b6a68c5e191b9e736068fed07441
alexsamaralns <alexsamaralns@gmail.com>
2020-08-11 10:14:07 -0300

    login screen

---
+++ src/views/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title></title>	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	<div class="box_login">
+		<div class="login-img">
+		</div>
+
+		<h1>Sing in</h1>
+
+		<form method="POST" action="#">
+			<input type="email" name="email" placeholder="E-mail">
+			<input type="password" name="password" placeholder="password">
+
+			<button class="btn-login">Login</button>
+
+			<span class="msg_error"><i class="fa fa-exclamation-triangle"></i> <p>Invalid login</p></span>
+		</form>
+	</div>
+</body>
+</html>
